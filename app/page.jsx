ec10b9bfee0f4ec8a7746b2ad34f9889e97f2be7
Caro-L-dev <liardcar@gmail.com>
2024-09-18 10:50:07 +0200

    feat: init repository

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="min-h-full flex justify-center items-center">
+      <button className="btn btn-primary">Click me</button>
+    </main>
+  );
+}
